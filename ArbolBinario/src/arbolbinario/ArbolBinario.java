e538acb4d394de1daaaf64e82e2f78a9e97a38cc
Alexis Jara <alexis.jara@unl.edu.ec>
2024-07-04 06:19:11 -0500

    GUI

--- ArbolBinario/src/arbolbinario/ArbolBinario.java
+++ ArbolBinario/src/arbolbinario/ArbolBinario.java
@@ -15,34 +15,6 @@ public class ArbolBinario {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        BinaryTree arbol = new BinaryTree(897);
-        
-        System.out.println(arbol);
-        arbol.addNode(578);
-        arbol.addNode(900);
-        arbol.addNode(901);
-        arbol.addNode(902);
-        arbol.addNode(450);
-        arbol.addNode(430);
-        arbol.addNode(455);
-        arbol.addNode(300);
-        arbol.addNode(992);
-        arbol.addNode(982);
-        System.out.println(arbol);
-        arbol.printTreeByLevels();
-        
-        
-        System.out.println("InOrder traversal:");
-        arbol.traverseInOrder();
-
-        System.out.println("PreOrder traversal:");
-        arbol.traversePreOrder();
-
-        System.out.println("PostOrder traversal:");
-        arbol.traversePostOrder();
     }
     
-    
-}
+   
--- ArbolBinario/src/arbolbinario/BinaryTree.java
+++ ArbolBinario/src/arbolbinario/BinaryTree.java
@@ -4,20 +4,57 @@
  */
 package arbolbinario;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+import java.util.function.Consumer;
 
 /**
  *
  * @author Alexis
  */
 public class BinaryTree {
-        private Node root;
+    private Node root;
     private int height;
     
     public BinaryTree(int value) {
         this.root = new Node(value);
         this.height = 1;
+    }
+    public boolean contains(int value) {
+        return containsValue(root, value);
+    }
+
+    private boolean containsValue(Node node, int value) {
+        if (node == null) {
+            return false;
+        }
+        if (node.getValue() == value) {
+            return true;
+        }
+        return containsValue(node.getLeftChild(), value) || containsValue(node.getRightChild(), value);
+    }
+    public Node getPadre(int valor) {
+        return encontrarPadre(root, valor);
+    }
+
+    private Node encontrarPadre(Node current, int valor) {
+        if (current == null) {
+            return null;
+        }
+
+        if ((current.getLeftChild() != null && current.getLeftChild().getValue() == valor) ||
+            (current.getRightChild() != null && current.getRightChild().getValue() == valor)) {
+            return current;
+        }
+
+        Node padre = encontrarPadre(current.getLeftChild(), valor);
+        if (padre == null) {
+            padre = encontrarPadre(current.getRightChild(), valor);
+        }
+
+        return padre;
     }
 
     public void addNode(int value) {
@@ -26,7 +63,9 @@ public class BinaryTree {
             height = calculateHeight(root);
         }
     }
-
+    public Node getRoot(){
+        return root;
+    }
     private int calculateHeight(Node node) {
         if (node == null) {
             return 0;
@@ -68,42 +107,50 @@ public class BinaryTree {
             }
         }
     }
-    public void traverseInOrder() {
-        inOrder(root);
-        System.out.println();
+    private void delay() {
+        try {
+            Thread.sleep(300); // Delay fijo de 300ms
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void traverseInOrder(Consumer<Node> action) {
+        inOrder(root, action);
     }
 
-    private void inOrder(Node node) {
+    private void inOrder(Node node, Consumer<Node> action) {
         if (node != null) {
-            inOrder(node.getLeftChild());
-            System.out.print(node.getValue() + " ");
-            inOrder(node.getRightChild());
+            inOrder(node.getLeftChild(), action);
+            action.accept(node);
+            delay(); // Delay de 300ms entre cada acción
+            inOrder(node.getRightChild(), action);
+        }
+    }
+ 
+
+    public void traversePreOrder(Consumer<Node> action) {
+        preOrder(root, action);
+    }
+
+    private void preOrder(Node node, Consumer<Node> action) {
+        if (node != null) {
+            action.accept(node);
+            delay(); // Delay de 300ms entre cada acción
+            preOrder(node.getLeftChild(), action);
+            preOrder(node.getRightChild(), action);
         }
     }
 
-    public void traversePreOrder() {
-        preOrder(root);
-        System.out.println();
+    public void traversePostOrder(Consumer<Node> action) {
+        postOrder(root, action);
     }
 
-    private void preOrder(Node node) {
+    private void postOrder(Node node, Consumer<Node> action) {
         if (node != null) {
-            System.out.print(node.getValue() + " ");
-            preOrder(node.getLeftChild());
-            preOrder(node.getRightChild());
-        }
-    }
-
-    public void traversePostOrder() {
-        postOrder(root);
-        System.out.println();
-    }
-
-    private void postOrder(Node node) {
-        if (node != null) {
-            postOrder(node.getLeftChild());
-            postOrder(node.getRightChild());
-            System.out.print(node.getValue() + " ");
+            postOrder(node.getLeftChild(), action);
+            postOrder(node.getRightChild(), action);
+            action.accept(node);
+            delay(); // Delay de 300ms entre cada acción
         }
     }
     @Override
